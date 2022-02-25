fec709352b19e3933ad25051a6ab289190032b5f
WENKAITAN <wenkaitan1019@gmail.com>
2022-02-24 22:00:03 -0500

    make it better

--- routes/product.js
+++ routes/product.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { Product } = require('../models')
-const Redis = require('redis')
+// const Redis = require('redis')
 
 // let redisClient;
 
